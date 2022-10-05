0df719f3599a4ca34e9bbd2e5535efbf655c6d13
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2022-10-04 20:18:25 -0700

    first JS code ever
    ha, and it is not working :)

---
+++ js/js.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// DO NOT REMOVE CODE ABOVE THIS LINE !
+
+// message = "hello world !"
+
+// console.log(message);
+
+// const message = "JavaScript is awesome!";
+// alert(message);
+
+// const username = "Mango";
+// console.log("Username is ", username); // Username is Mango
+
+// const message = "JavaScript is awesome!";
+// console.log(message); // JavaScript is awesome!
+
+
+console.log(Number.parseFloat("5px")); // 5
+console.log(Number.parseFloat("12qwe74")); // 12
+console.log(Number.parseFloat("12.46qwe79")); // 12.46
+console.log(Number.parseFloat("qweqwe")); // NaN
