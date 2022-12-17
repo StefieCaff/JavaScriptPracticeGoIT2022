d4a754fc4c89ae38ee2339ac364e4e6acbd5d8e6
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2022-12-16 22:27:33 -0800

    update practice script

--- js/script.js
+++ js/script.js
@@ -1013,3 +1013,30 @@
 
 // console.log(names);
 
+
+class Storage{
+  constructor([items]) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    
+  }
+
+  removeItem(itemToRemove) {
+    
+  }
+}
+
+
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
